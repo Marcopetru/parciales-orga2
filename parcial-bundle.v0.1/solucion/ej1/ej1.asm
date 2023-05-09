2167ca9f6e870c9bd8095bb69b89e87a31103815
Marcopetru <36896969+Marcopetru@users.noreply.github.com>
2023-05-09 16:30:49 -0300

    Update ej1.asm

--- parcial-bundle.v0.1/solucion/ej1/ej1.asm
+++ parcial-bundle.v0.1/solucion/ej1/ej1.asm
@@ -32,29 +32,37 @@ sub rsp, 8
 mov r13, rdi ; me guardo los datos en otros registros
 mov r14, rsi
 
+;(comentario post parcial) aca es donde hubiese tenido que pedir memoria basandome en la funcion cuantosTemplosClasicos(call), mover el rax a rdi para llamar a malloc
+;y multiplicar el puntero de templos clasicos *24 porque cada struct es de 24 bytes
 
     .ciclo:
         mov r13, [rdi + OFFSET_COLUM_LARGO];  en 0
-        mov r14, [rsi + OFFSET_COLUM_CORTO];  ¨¨¨¨
+        mov r14, [rsi + OFFSET_COLUM_CORTO];  ¨¨¨¨        ;(comentario post parcial)tendria que ser r13 en vez de rdi y rsi y guardar en otro registro que no sea r13
 
         ;filtro para templos clasicos: colum largo = 2*clomun corto + 1
         mov r15, [2*r14 + 1]
 
-        cmp rax, [SIZE_STRUCT]
-        je .fin
+        cmp rax, [SIZE_STRUCT]  ;(comentario post parcial)no se bien que intente hacer aca 
+        je .fin                 ;(""""""""""""""""""""""")""""""""""""""""""""""""""""""""
         cmp r13, r15
         je .esTemploclasico
         jmp .sigue
 
         .esTemploclasico: ;agrego el nombre al nuevo array de templos clasicos
         movzx eax, byte [OFFSET_NOMBRE] ;muevo al nuevo array de templos clasicos el nombre del templo sin pisar el resto del registro 
-        mov rax, [rax + OFFSET_NOMBRE]
+        mov rax, [rax + OFFSET_NOMBRE]  ;(comentario post parcial) hubiese tenido que ser: mov rdx, [r13 + OFFSET_NOMBRE]
+                                        ;                                                   mov r8(o cualquier registro), [r13 + OFFSET_COLUM_LARGO]
+                                        ;                                                   mov r9"", [r13 + OFFSET_COLUM_CORTO]
+                                        ;                                                   mov [rax + OFFSET_COLUMN_LARGO], r13
+                                        ;                                                   mov [rax + OFFSET_NOMBRE], r8
+                                        ;                                                   mov [rax + OFFSET_COLUMN_CORTO], r9
+                                        ;                                                   add rax, SIZE_STRUCT
         jmp .ciclo
         
         .sigue: ;continua sin agregar el nombre ya que no era clasico
         mov rax, [rax + OFFSET_NOMBRE]
         mov r13, [r13 + OFFSET_COLUM_LARGO]
-        mov r14, [r14 + OFFSET_COLUM_CORTO]
+        mov r14, [r14 + OFFSET_COLUM_CORTO]    ;(comentario post parcial) aca solo hacer el add y loopear el ciclo(osea las ultimas dos lineas de .sigue)
         add rdi, SIZE_STRUCT 
         jmp .ciclo
             
@@ -62,11 +70,11 @@ mov r14, rsi
 
 .fin:
 call malloc
-mov r8, [rax + OFFSET_NOMBRE]
+mov r8, [rax + OFFSET_NOMBRE]                ;(comentario post parcial) basicamente no sabia como pedir memoria entonces pense en esto que esta mal claramente
 mov [rax + OFFSET_COLUM_LARGO], r13
 mov [rax + OFFSET_COLUM_CORTO], r14
     
-pop r8
+pop r8                                       ;(comentario post parcial) esto esta mal aca deberia mover el rax que debi guardar antes de ciclar
 
 
 ;uint32_t cuantosTemplosClasicos(templo *temploArr, size_t temploArr_len)
@@ -92,13 +100,13 @@ mov r10, NULL
 
     .ciclo1:
         mov rax, [OFFSET_NOMBRE]
-        mov r13,  [rdi + OFFSET_COLUM_LARGO]
+        mov r13,  [rdi + OFFSET_COLUM_LARGO]  ;(comentario post parcial) lo mismo que comente en la funcion anterior
         mov r14,  [rsi + OFFSET_COLUM_CORTO] 
 
         ;filtro para templos clasicos: colum largo = 2*clomun corto + 1
         mov r15, [2*r14 + 1]
 
-        cmp rax, [SIZE_STRUCT]
+        cmp rax, [SIZE_STRUCT]     ;(comentario post parcial) lo mismo que comente antes
         je .fin1
         cmp r13, r15
         je .esTemploclasico1
@@ -106,34 +114,34 @@ mov r10, NULL
 
         .esTemploclasico1: ;va sumando 1 al registro r10 que esta en 0 segun sea clasico el templo y pasa al siguiente nombre
         add r10, 1
-        mov rax, [rax + OFFSET_NOMBRE]
+        mov rax, [rax + OFFSET_NOMBRE]       ;(comentario post parcial) aca solo necesitaba incrmentar r10
         mov r13, [r13 + OFFSET_COLUM_LARGO]
         mov r14, [r14 + OFFSET_COLUM_CORTO]
         jmp .ciclo1
 
         .noesteploclasico: ;no suma nada al registro r10 y pasa al siguiente nombre 
         add r10, 0 
-        mov rax, [rax + OFFSET_NOMBRE]
+        mov rax, [rax + OFFSET_NOMBRE]           ;(comentario post parcial) aca solo tenia que hacer ; add r13, SIZE_STRUCT y loopear el ciclo
         mov r13, [r13 + OFFSET_COLUM_LARGO]
-        mov r14, [r14 + OFFSET_COLUM_CORTO]
+        mov r14, [r14 + OFFSET_COLUM_CORTO]       
         jmp .ciclo1
 
         loop .ciclo1
 
 .fin1:
 call malloc
-mov r8, [rax + OFFSET_NOMBRE]
+mov r8, [rax + OFFSET_NOMBRE]               ;(comentario post parcial) lo mismo que comente antes, deberia haber llamado a malloc anteriormente para pedir memoria para estanfuncion
 mov [rax + OFFSET_COLUM_LARGO], r13
 mov [rax + OFFSET_COLUM_CORTO], r14
 
-pop r10
+pop r10                                      ;(comentario post parcial) aca mov rax, r10
 
 
 ;epilogo---------------------------------------------------------------------------------------
 
-add rsp, 8
+add rsp, 8                                      ;(comentario post parcial) toda esta parte del epilogo deberia ir antes de cuantosTemplosClasicos
 pop r13
 pop r14
-pop r15    ;restauro los registros no volatiles
+pop r15    ;restauro los registros no volatiles ;(comentario post parcial) hasta aca 
 pop rbp
 ret
